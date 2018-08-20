085267c1f3de1a56375ff1c36f9846d13e3a838f
Yuki Miyamoto <yumiya0403two@gmail.com>
2018-08-20 10:21:35 +0900

    npmのfirebaseライブラリとコンフィグを追加してfirebaseを 使えるようにした。タグを付けてTypeScript+React+Firebaseの テンプレートとして使えるようにする。

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,33 @@
-import * as React from 'react';
-import './App.css';
-
-import logo from './logo.svg';
+import * as firebase from "firebase";
+import * as React from "react";
+import "./App.css";
+import logo from "./logo.svg";
 
 class App extends React.Component {
+  constructor(props: any) {
+    super(props);
+
+    // Firebaseの初期化
+    const config = {
+      apiKey: "AIzaSyCssqXrGArSzeAqqQ7U2yKgputcCozXVzQ",
+      authDomain: "my-custom-info.firebaseapp.com",
+      databaseURL: "https://my-custom-info.firebaseio.com",
+      messagingSenderId: "948285868812",
+      projectId: "my-custom-info",
+      storageBucket: "my-custom-info.appspot.com"
+    };
+    firebase.initializeApp(config);
+  }
+
+  public componentDidMount() {
+    firebase
+      .database()
+      .ref()
+      .once("value", snap => {
+        console.log(snap.val());
+      });
+  }
+
   public render() {
     return (
       <div className="App">
